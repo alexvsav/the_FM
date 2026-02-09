e9891ebb98fcef0b2e667231df5974cf218df2f9
Alexandre <68223583+alexvsav@users.noreply.github.com>
2026-02-09 21:10:48 +0100

    Update fuck-map.jsx

--- src/fuck-map.jsx
+++ src/fuck-map.jsx
@@ -4,8 +4,16 @@ import { Share2, Trophy, MapPin, Info, X, RotateCcw, AlertTriangle, Scale, Shiel
 
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { 
+  getDatabase, 
+  ref, 
+  set, 
+  onValue,
+  query,
+  orderByChild,
+  limitToLast 
+} from 'firebase/database';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
