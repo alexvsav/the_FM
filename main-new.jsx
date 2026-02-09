dd29f9ca9b8d45405e1ccae8d159b27db4249921
Alexandre <68223583+alexvsav@users.noreply.github.com>
2026-02-09 21:00:16 +0100

    Delete main-new.jsx

--- main-new.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import FuckMap from './fuck-map.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <FuckMap />
-  </React.StrictMode>
-)
